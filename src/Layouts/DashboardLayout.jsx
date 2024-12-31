2e436e63eefdaf7c0f31c366ff4979068daff797
SI-tanbir <si017490@gmail.com>
2024-12-31 14:28:38 +0600

    i accidentlaly make wronkg left side now it fixed and i added the active class css funtion with react navlink

--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router-dom"; // Use NavLink for active class
 import { FaHome } from "react-icons/fa";
 import { PiForkKnifeFill } from "react-icons/pi";
 import { HiMenu } from "react-icons/hi";
@@ -9,72 +9,54 @@ import { RiShoppingBag4Fill } from "react-icons/ri";
 import { MdContacts } from "react-icons/md";
 
 const DashboardLayout = () => {
+  const navItems = [
+    { name: "User Home", icon: <FaHome />, to: "/dashboard/home" },
+    {
+      name: "Reservation",
+      icon: <PiForkKnifeFill />,
+      to: "/dashboard/reservation",
+    },
+    {
+      name: "Payment History",
+      icon: <HiMenu />,
+      to: "/dashboard/payment-history",
+    },
+    { name: "MY Carts", icon: <FaBook />, to: "/dashboard/carts" },
+    { name: "ADD Review", icon: <PiUsersThreeFill />, to: "/dashboard/review" },
+    {
+      name: "MY BOOKINGS",
+      icon: <PiUsersThreeFill />,
+      to: "/dashboard/bookings",
+    },
+    { name: "Home", icon: <FaHome />, to: "/" },
+    { name: "Menu", icon: <HiMenu />, to: "/dashboard/menu" },
+    { name: "Shop", icon: <RiShoppingBag4Fill />, to: "/dashboard/shop" },
+    { name: "Contact", icon: <MdContacts />, to: "/dashboard/contact" },
+  ];
+
   return (
     <div className="flex">
-      <div className="bg-slate-300 w-[250px] p-4 h-[100vh] ">
+      <div className="bg-slate-300 w-[250px] p-4 h-[100vh]">
         <ul>
-          <li className="hover:bg-white p-2 rounded-md  flex  ">
-            <FaHome className="text-2xl mx-2" />
-            <Link to={"/dashboard/home"}> Admin Home</Link>
-          </li>
-
-          <li className="hover:bg-white p-2 rounded-md  flex">
-            <PiForkKnifeFill className="text-2xl mx-2" />
-
-            <Link to={""}>Add Items</Link>
-          </li>
-
-          <li className="hover:bg-white p-2 rounded-md  flex">
-            <HiMenu className="text-2xl mx-2" />
-
-            <Link to={"/dashboard/home"}>Manage Items</Link>
-          </li>
-
-          <li className="hover:bg-white p-2 rounded-md  flex">
-            <FaBook className="text-2xl mx-2" />
-
-            <Link to={"/dashboard/home"}>Manage Bookings</Link>
-          </li>
-
-          <li className="hover:bg-white p-2 rounded-md  flex">
-            <PiUsersThreeFill className="text-2xl mx-2" />
-
-            <Link to={"/dashboard/home"}>All Users</Link>
-          </li>
-
-          <hr />
-
-          <li className="hover:bg-white p-2 rounded-md  flex">
-            <FaHome className="text-2xl mx-2" />
-
-            <Link to={"/dashboard/home"}>Home</Link>
-          </li>
-
-          <li className="hover:bg-white p-2 rounded-md  flex">
-            <HiMenu className="text-2xl mx-2" />
-
-            <Link to={"/dashboard/home"}>Menu</Link>
-          </li>
-
-          <li className="hover:bg-white p-2 rounded-md  flex">
-            <RiShoppingBag4Fill className="text-2xl mx-2" />
-
-            <Link to={"/dashboard/home"}>Shop</Link>
-          </li>
-
-          <li className="hover:bg-white p-2 rounded-md  flex">
-            <MdContacts className="text-2xl mx-2" />
-
-            <Link to={"/dashboard/home"}>Contact</Link>
-          </li>
+          {navItems.map((item) => (
+            <li key={item.name} className="p-2 rounded-md">
+              <NavLink
+                to={item.to}
+                className={({ isActive }) =>
+                  `flex items-center space-x-2 p-2 rounded-md ${
+                    isActive ? "bg-orange-500 text-white" : "hover:bg-white"
+                  }`
+                }
+              >
+                {item.icon}
+                <span>{item.name}</span>
+              </NavLink>
+            </li>
+          ))}
         </ul>
       </div>
-
       <div>
-        {/* Right side bar */}
-        {/* */}
-
-        <Outlet></Outlet>
+        <Outlet />
       </div>
     </div>
   );
